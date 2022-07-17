3285682207f33a776382eff4d6b57b00a04ecff0
Hinrich <johnhinrichggalindo@gmail.com>
2022-07-17 17:33:43 +0800

    removed quickSort from algorithms.js module

--- js/functions/algorithms.js
+++ js/functions/algorithms.js
@@ -52,9 +52,6 @@ export const bubbleSort = (arr = [8,5,12,4,32,65,32], isAscending = true, contro
     return arr
 }
 
-const quickSort = (arr) => {
-
-}
 
 const insertionSort = (arr) => {
 
@@ -63,33 +60,3 @@ const insertionSort = (arr) => {
 const mergeSort = (arr) => {
 
 }
-
-export function quicksort(options) {
-    const {array, isAscending, controlVar} = options
-    if (array.length <= 1) {
-      return array;
-    }
-
-    let pivot = array[0]
-    console.log(controlVar )
-    
-    const left = []; 
-    const right = [];
-    
-    
-    for (let i = 1; i < array.length; i++) {
-        // console.log(array[i])
-      array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
-    }
-
-    const quickSortLeft = {array: left, isAscending: isAscending, controlVar: controlVar}
-    const quickSorRight = {array: right, isAscending: isAscending, controlVar: controlVar}
-
-    return quicksort(quickSortLeft).concat(pivot, quicksort(quickSorRight));
-};
-  
-  var unsorted = [23, 45, 16, 37, 3, 99, 22];
-  var unsortedd = [[23], [45, 16], [37, 3, 99], [22]];
-  var sorted = quicksort({array:unsorted, isAscending:true, controlVar:null});
-  
-  console.log('Sorted array', sorted);
