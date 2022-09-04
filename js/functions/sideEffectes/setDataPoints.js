4997e0edb1eaa83cebbe73f54e3b1dc8c098aefa
Hinrich <johnhinrichggalindo@gmail.com>
2022-09-04 14:34:13 +0800

    added comment describing the effect of setDataPoints

--- js/functions/sideEffectes/setDataPoints.js
+++ js/functions/sideEffectes/setDataPoints.js
@@ -1,3 +1,8 @@
+/*
+    Takes the first line of the given CSV file as header names.
+    For each header name, it will add an <option/> element to the selectElement.
+*/
+
 const setDataPoints = (results, selectElement) => {
     const csvHeaders = results.data[0].map(element => `<option>${element}</option>`);
     selectElement.innerHTML = csvHeaders.join('');
