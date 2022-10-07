2257428698172f1f3af3557223bf209d6f7fb79a
Hinrich <johnhinrichggalindo@gmail.com>
2022-10-07 20:06:40 +0800

    updated the on update function

--- main.js
+++ main.js
@@ -1,94 +1,181 @@
-// import './style.css'
-import { parse } from 'papaparse'
+import CSV from './js/classes/Csv.js';
+import STATUS from './js/classes/Status.js';
+import State from './js/classes/state.js';
 
-const table = document.querySelector('table');
-const tableHeaders = document.querySelector('#headers');
-const tableDatas = document.querySelector('#datas');
+// algorithms
+import quickSort from './js/functions/algo/quickSort.test.js';
 
-const getCSVData = async (file) => {
-  const csv = await fetch(file)
-  const text = await csv.text();
-  console.log('parse:', parse(text.trimEnd()));
-  return parse(text);
+// side effect funtions
+import setDataPoints from './js/functions/sideEffectes/setDataPoints.js';
+import htmlToCSV, {test, downloadCSVFile } from './js/functions/sideEffectes/htmlToCSV.js';
+
+// pure functions
+// import onDisplay from './js/functions/sideEffectes/onDisplay.js';
+// import { realValParser } from './js/functions/parsing/numbers.js';
+
+const form = document.querySelector("#getfile");
+const selectGroup = document.querySelector('#sort-select-group')
+const select = document.querySelector('#select');
+
+// class instance
+const state = new State();
+const csv = new CSV(document.querySelector('table'))
+const Status = new STATUS(document.querySelector('#status'));
+
+//buttons
+import {
+  clearBtn,
+  displayBtn,
+  stopBtn,
+  inputFile,
+  submitBtn,
+  updateBtn,
+  downloadBtn
+} from './js/buttons.js';
+
+// predefined options
+const onChooseFileOptions = {
+  hideBtn:[clearBtn, displayBtn, stopBtn],
 }
 
-const displayCSV = async (file) => {
-  const result = await getCSVData(file);
-  const data = result.data
+const onDoneOptions = {
+  hideBtn:[displayBtn, stopBtn],
+  showBtn: [clearBtn]
+}
 
-  const headers = data[0];
-  // console.log(headers);
-  headers.forEach(header => {
-    const td = document.createElement('td');
-    td.innerText = header;
-    tableHeaders.appendChild(td);
-  })
+const onSetFileOptions = {
+  hideBtn:[clearBtn, stopBtn],
+  showBtn: [displayBtn, selectGroup]
+}
 
-  // remove all undefineds
-  const datas = removeUndefined(data.slice(35000));
-  console.log(datas)
+const onLoadingOptions = {
+  hideBtn:[clearBtn, displayBtn]
+}
 
-  //remove all comma and parse to float
-  console.log(parseFloat("733,258".replace(/,/g, '')))
+// initial state
+Status.onChooseFile();
+Status.Options.hide([selectGroup]);
+Status.Options.disable([stopBtn, clearBtn, displayBtn, updateBtn, downloadBtn]);
 
-  // datas.sort((a,b) => parseInt(a[8]) - parseInt(b[8]));
+// configs
 
-  const parsedDatas = datas.map( data => {
-    // if(data[8] == "C") {
-    //   console.log(datas.indexOf(data));
-    //   console.log(data)
-    // }
-    data[8] = {
-      original: data[8],
-      parsed: parseFloat(data[8].replace(/,/g, ''))
+const sortingAlgorithm = (...args) => {
+  return quickSort(...args)
+};
+
+const displayMethod = (...args) => {
+  csv.onSummarize(...args)
+}
+
+let selectedFile;
+
+// buttons on click
+
+// on submit state
+form.onsubmit = async e => {
+
+  e.preventDefault();
+
+  selectedFile = inputFile.files[0];
+
+  if(!selectedFile) return;
+
+  csv.clear();
+
+  Status.onSetFile(selectedFile.name, onSetFileOptions);
+
+  Papa.parse(selectedFile, {
+
+    complete: results => {
+      setDataPoints(results, select);
     }
-    return data;
-  })
 
-
-  // console.log(parsedDatas.map(data => [data[8], data[9]]))
-  console.log(parsedDatas.map(data => data[8]).indexOf("C") )
-  // console.log(parsedDatas.map(data => data[8]).filter(value => value.original == "C"))
-  console.log('datas',datas.map(data => data[8]).filter(value => value.original == "C"))
-  // console.log(parsedDatas.map(data => [data[8], data[9]]).find(value => value[0].original == "C"))
-  // console.log(datas.map(data => [data[8], data[9]]))
-  
-  parsedDatas.forEach(row => {
-
-    const tr = document.createElement('tr');
-    row.forEach( data => {
-      const td = document.createElement('td');
-      if(data.original) {
-        td.innerText = data.original;
-      }else {
-        td.innerText = data;
-      }
-      // tableDatas.appendChild(td);
-      tr.appendChild(td)
-      // console.log(data)
-    })
-    table.appendChild(tr)
   });
-  console.log(data)
+
+  // results = await fileParse(selectedFile);
+
+  Status.Options.disable([stopBtn, clearBtn])
+
+  Status.Options.show([selectGroup])
+
+  Status.Options.enable([displayBtn])
+  
+  form.reset();
 }
-displayCSV('annual-enterprise-survey-2020-financial-year-provisional-csv.csv')
-// displayCSV('test.csv')
-// getCSV('annual-enterprise-survey-2020-financial-year-provisional-csv.csv')
 
+// displayBtn initiate's the loading state
+displayBtn.onclick = () => {
+  Status.onLoading(onLoadingOptions);
 
+  Papa.parse(selectedFile, {
+    worker: true,
 
-const loop  = async () => {
-  let x = 0
-  while(x > 50000) {
-    console.log(x);
-    x++;
+    // Header: true,
+
+    complete: results => {
+      const removeUndefined = data => data.filter(element => element !== undefined && element != '');
+
+      
+      const headerColumn = results.data[0];
+      
+      const csvBody = results.data.slice(1)
+
+      const bodyData = removeUndefined(csvBody)  
+
+      // addToConfig -> settings for what algorithm to use
+      displayMethod(headerColumn, csvBody)
+
+      const algorithmConfig = {
+
+      }
+      
+      // button state handling
+      Status.Options.hide([selectGroup])
+      Status.Options.disable([clearBtn, displayBtn, submitBtn, inputFile])
+      Status.Options.enable([stopBtn, updateBtn])
+
+      updateBtn.onclick = () => {
+        onUpdate(headerColumn, csvBody)
+        Status.Options.enable([downloadBtn]);
+      }
+    }
+
+  });
+};
+
+// on clear
+clearBtn.onclick = () => {
+  if(document.querySelector('.downloadBtn')) {
+      document.querySelector('.downloadBtn').remove()
   }
+
+  csv.clear();
+  Status.onChooseFile(onChooseFileOptions);
+  Status.Options.hide([selectGroup])
+  Status.Options.disable([clearBtn, stopBtn])
+  select.innerHTML = '';
 }
 
-const stop = (task) => {
-  setTimeout(task);
+const onUpdate = (headerColumn, csvBody) => {
+
+  // console.log(convertedBodyData)
+  // console.log(quickSort(config))
+  // let sorted = quickSort(config)
+  // csv.summarize(results.data[0], sorted)
+  let sorted = sortingAlgorithm(csvBody, select.selectedIndex);
+  console.log(sorted)
+  displayMethod(headerColumn, sorted)
+
+
+          
+  // test download button
+  if(document.querySelector('.downloadBtn')) return;
+
+  downloadBtn.onclick = () => {
+    var html = document.querySelector("table").outerHTML;
+    // console.log(results.data[0].join(","))
+    // test(results.data[0],sorted, `Sorted-by-${select.value}-${firstFile.name}`, downloadCSVFile);
+
+    test(headerColumn, sorted, `Sorted-by-${select.value}-${selectedFile.name}`, downloadCSVFile);
+  } 
 }
-
-document.querySelector('button').onclick = stop(loop);
-
-const removeUndefined = data => data.filter(element => element !== undefined && element != '');
