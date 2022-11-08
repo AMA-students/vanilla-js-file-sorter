f61e33727b2c5ec7b3c667fe168ac92e0b257b0a
Hinrich <johnhinrichggalindo@gmail.com>
2022-11-08 18:31:41 +0800

    added selectionSort

---
+++ js/functions/algo/selectionSort.js
@@ -0,0 +1,49 @@
+import { alphanumericComparator, stringToNumber } from "../../classes/utility.js"
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      let lowest = i
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[lowest]) {
+          lowest = j
+        }
+      }
+      if (lowest !== i) {
+        // Swap
+        ;[arr[i], arr[lowest]] = [arr[lowest], arr[i]]
+      }
+    }
+    return arr
+}
+
+function selectionSortCSV(arr, dataPointIndex) {
+  for (let i = 0; i < arr.length; i++) {
+
+    // base lowest
+    let lowest = i
+
+    // for loop for determining the actual lowest
+    for (let j = i + 1; j < arr.length; j++) {
+      const currentValue = stringToNumber(arr[j][dataPointIndex]).realVal;
+      const currentLowest = stringToNumber(arr[lowest][dataPointIndex]).realVal;
+
+      if ( currentValue < currentLowest ) {
+        lowest = j
+      }
+    }
+
+    if (lowest !== i) {
+      // Swap
+      ;[arr[i], arr[lowest]] = [arr[lowest], arr[i]]
+    }
+  }
+  return arr
+}
+
+
+// console.log(selectionSort([3, 5, 1, 2])) // [1, 2, 3, 5]
+const unsorted2 = [[45, 16],[23,42], [37, 3], [22,43]];
+// console.log(selectionSortCSV(unsorted2,0));
+export {
+  selectionSortCSV,
+}
