d7fc902eb66117d30e7c0a7826320575d79f3418
Hinrich <johnhinrichggalindo@gmail.com>
2023-01-14 23:42:46 +0800

    removed the duplicate code for headerHilighter()

--- js/classes/TableController.js
+++ js/classes/TableController.js
@@ -1,16 +1,21 @@
 export default class {
-    constructor(root) {
-        this.root = root
-    }
+  constructor(root) {
+      this.root = root
+  }
 
-    headerHighlighter = (headerIndex) => {
-      const headers = this.root.querySelectorAll('th');
-        headers.forEach((header, index) => {
-          if(index === headerIndex) {
-            header.classList.add('highlight')
-          } else {
-            header.classList.remove('highlight')
-          }
-        })
-    }
+  headerHighlighter = (headerIndex) => {
+
+    const headers = this.root.querySelectorAll('th');
+    headers.forEach((header, index) => {
+
+      if(index === headerIndex) {
+        header.classList.add('highlight')
+      } else {
+        header.classList.remove('highlight')
+      }
+
+    })
+
+  }
+  
 }
