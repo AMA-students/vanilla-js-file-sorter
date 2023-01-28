33b4ed2cc894d6d535b776c6a4098e7790a70f1e
Hinrich <johnhinrichggalindo@gmail.com>
2023-01-28 19:04:24 +0800

    removed the tests for bubbleSort

--- js/functions/data-based-sorters/bubbleSort.js
+++ js/functions/data-based-sorters/bubbleSort.js
@@ -1,12 +1,4 @@
-// import {alphanumericComparator, isValidNumberButWithCommaValidator} from "../../classes/utility.js";
-
-import DataRecorder from "../../classes/DataRecorder.js";
-
-import { alphanumericComparator, sortingMode, removeUndefined } from "../../classes/utility.js"
-
-import { CSVRecorder } from "../../classes/FileRecorders.js"
-
-import { CSVParser, fileParse } from "../../classes/CSVParser.js"
+import { alphanumericComparator, sortingMode } from "../../classes/utility.js"
 
 const bubbleSort = (array, dataPointIndex, dataRecorder) => {
     
@@ -121,60 +113,6 @@ const mode = (currentX, currentY, dataPointIndex) => {
         currentX,
         currentY
     ] = isValidNumberButWithCommaValidator(currentX[dataPointIndex], currentY[dataPointIndex]);
-}
-
-// const [arrayTest, basicTest, dementionalArrayTest] = false
-const [arrayTest, basicTest, dementionalArrayTest] = [false, false, false]
-
-const fileTest = true;
-const sortingMethod = bubbleSort;
-
-if(arrayTest) {
-    
-    const unsorted2 = [[45, 16],[23,42], [37, 3], [22,43]];
-
-    const unsorted3 = unsorted2.map(elem => elem[1]);
-
-    if(basicTest) {
-        console.log(sortingMethod([3, 5, 1, 2])) // [1, 2, 3, 5]
-        console.log(sortingMethod(unsorted3));
-    }
-
-    
-    
-    if(dementionalArrayTest) {
-        console.log(sortingMethod(unsorted2, 0));
-        console.log(sortingMethod(unsorted2, 1));
-    }
-
-}
-
-if(fileTest) {
-    fileParse('./8.csv','\n').then(data => {
-
-        data = removeUndefined(data)
-    
-        const dataRecorder = new CSVRecorder();
-    
-        let CSV = removeUndefined(CSVParser(data))
-    
-        dataRecorder.initializeFileContent(data.join('\n'))
-    
-        dataRecorder.initializeParsedFileContent(CSV)
-    
-        dataRecorder.initializeFileContentRecords()
-    
-        dataRecorder.initializeDatapointIndex(0)
-    
-        const dataPointIndex = 0;
-    
-        console.log(
-            sortingMethod(dataRecorder.fileContentRecords, dataPointIndex, dataRecorder)
-        );
-            
-        dataRecorder.initializeSortedFileContent()
-
-    });
 }
 
 export default bubbleSort;
