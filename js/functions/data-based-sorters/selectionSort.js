576a3408b88b10c42cc361d789a97d30319be457
Hinrich <johnhinrichggalindo@gmail.com>
2023-01-28 18:58:27 +0800

    modified the selectionSort to sort fileContentRecords

--- js/functions/data-based-sorters/selectionSort.js
+++ js/functions/data-based-sorters/selectionSort.js
@@ -1,6 +1,11 @@
-import { alphanumericComparator, sortingMode } from "../../classes/utility.js"
+import { alphanumericComparator, sortingMode, removeUndefined } from "../../classes/utility.js"
 
-function selectionSort(arr, dataPointIndex) {
+import { CSVRecorder } from "../../classes/FileRecorders.js"
+
+import { CSVParser, fileParse } from "../../classes/CSVParser.js"
+
+function selectionSort(arr, dataPointIndex, dataRecorder) {
+
   for (let i = 0; i < arr.length; i++) {
 
     // base lowest
@@ -11,28 +16,42 @@ function selectionSort(arr, dataPointIndex) {
 
     // for loop for determining the actual lowest
     for (let j = i + 1; j < arr.length; j++) {
+    
+    let currentValue, currentLowest;
 
-      const [currentValue, currentLowest] = sortingMode(arr[j], arr[lowest], dataPointIndex);
+    [currentValue, currentLowest] = sortingMode(
+        arr[j],
+        arr[lowest],
+        dataPointIndex,
+        dataRecorder
+    );
 
-      if(typeof(currentValue) === 'string' || typeof(currentLowest) === 'string') {
+
+
+    //   const [currentValue, currentLowest] = sortingMode(arr[j], arr[lowest], dataPointIndex);
+
+    if(typeof(currentValue) === 'string' || typeof(currentLowest) === 'string') {
 
         if(alphanumericComparator(currentLowest, currentValue, collator)) {
           lowest = j
         }
 
-        continue;
-      }
+            continue;
+        }
 
-      if ( currentValue < currentLowest ) {
-        lowest = j
-      }
+        if ( currentValue < currentLowest ) {
+            lowest = j
+        }
     }
 
+    
     if (lowest !== i) {
-      // Swap
-      [arr[i], arr[lowest]] = [arr[lowest], arr[i]]
+        // Swap
+        [arr[i], arr[lowest]] = [arr[lowest], arr[i]]
     }
   }
+
+  dataRecorder?.initializeSortedParsedFileContent()
   return arr
 }
 
@@ -41,7 +60,37 @@ const unsorted2 = [[45, 16],[23,42], [37, 3], [22,43]];
 
 const unsorted3 = unsorted2.map(elem => elem[1]);
 
-console.log(selectionSort(unsorted3));
+// console.log(selectionSort(unsorted2, 1));
+
+if(true) {
+    fileParse('./8.csv','\n').then(data => {
+
+        data = removeUndefined(data)
+    
+        const dataRecorder = new CSVRecorder();
+    
+        let CSV = removeUndefined(CSVParser(data))
+    
+        dataRecorder.initializeFileContent(data.join('\n'))
+    
+        dataRecorder.initializeParsedFileContent(CSV)
+    
+        dataRecorder.initializeFileContentRecords()
+    
+        dataRecorder.initializeDatapointIndex(0)
+    
+        const array = [...dataRecorder.parsedFileContentBody];
+    
+        const dataPointIndex = 0;
+    
+        console.log(
+            selectionSort(dataRecorder.fileContentRecords, dataPointIndex, dataRecorder)
+        );
+            
+        dataRecorder.initializeSortedFileContent()
+
+    });
+}
 
 export {
   selectionSort,
