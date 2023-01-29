8c5e7cb15187d8e9adae450f41327f48a81e8958
Hinrich <johnhinrichggalindo@gmail.com>
2023-01-29 23:38:33 +0800

    deleted the Button.js class

--- js/classes/Buttons.js
+++
@@ -1,7 +0,0 @@
-const displayBtn = document.querySelector('#display')
-const clearBtn = document.querySelector('#clear')
-const stopBtn = document.querySelector('#stop')
-
-export default class Buttons {
-    
-}
