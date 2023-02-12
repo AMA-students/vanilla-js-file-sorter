c45bcc17e128347f96766d447125dd2102538d9c
Hinrich <johnhinrichggalindo@gmail.com>
2023-02-12 14:08:13 +0800

    added JSONRecorder, traverser(), dataRecorderSetter, and added type property to CSVRecorder and JSONRecorder

--- js/classes/FileRecorders.js
+++ js/classes/FileRecorders.js
@@ -10,6 +10,8 @@ class CSVRecorder extends DataRecorder {
     constructor(fileContent, datapointIndex) {
         super();
 
+        this.type = "CSV";
+
         this.initializeFileContent(fileContent)
         
         this.initializeDatapointIndex(datapointIndex)
@@ -77,6 +79,105 @@ class CSVRecorder extends DataRecorder {
     
 }
 
+class JSONRecorder extends DataRecorder {
+
+    constructor(fileContent, datapointIndex) {
+        
+        // super(fileContent, datapointIndex);
+        super();
+
+        this.type = "JSON";
+
+        this.initializeFileContent(fileContent)
+        
+        this.initializeDatapointIndex(datapointIndex)
+    }
+
+    initializeFileContent(fileContent) {
+        this.setFileContent(fileContent)
+
+        // if(!this.fileContent) return null;
+
+        // this.parsedFileContent = JSON.parse(this.fileContent)
+
+        // if(!this.splitFileContent) return null;
+        // this.setFileContentHeader(this.splitFileContent[0])
+        // this.setFileContentBody(this.splitFileContent.slice(1))
+
+        // this.initializeFileContentRecords()
+    }
+
+    initializeDatapointIndex() {
+
+    }
+
+    initializeParsedFileContent() {
+        if(!this.fileContent) return;
+
+        this.parsedFileContent = JSON.parse(this.fileContent);
+
+        // if(!Array.isArray(this.parsedFileContent)) return;
+        // console.log(this.parsedFileContent, Array.isArray(this.parsedFileContent.people));
+
+    }
+
+    initializeFileContentRecords() {
+        // if(!this.parsedFileContentBody) return null;
+        // this.setFileContentRecords()
+
+        // if(!this.fileContentRecords) return null;
+        // this.setFileContentLines()
+        
+        // if(!this.datapointIndex) return null;
+        // this.setFileContentValues()
+    }
+
+    getKeys(obj) {
+        let keys = [];
+      
+        for (let key in obj) {
+          keys.push(key);
+      
+          if (typeof obj[key] === "object") {
+            keys = keys.concat([this.getKeys(obj[key])]);
+          }
+        }
+        console.log(keys);
+        return keys;
+    }
+
+}
+
+function traverser(obj, propertyPath, level = 0) {
+
+    const pathLenth = propertyPath.length - 1;
+
+    const newObj = obj[propertyPath[level]];
+
+    if(level === pathLenth) return newObj;
+
+    level++;
+
+    return traverser(newObj, propertyPath, level)
+}
+
+const dataRecorderSetter = (fileName) => {
+
+    const fileExtension = fileName.match(/[^.]*(?=$)/)
+
+    const fileRecorders = {
+        csv: () => new CSVRecorder(),
+        json: () => new JSONRecorder()
+    }
+
+    return fileRecorders[fileExtension]();
+}
+
+
+
 export {
-    CSVRecorder
+    traverser,
+    CSVRecorder,
+    JSONRecorder,
+    dataRecorderSetter
 }
