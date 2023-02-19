e78d557d8f20cc8f21c9bb868552222b42aa98ff
Hinrich <johnhinrichggalindo@gmail.com>
2023-02-19 13:10:58 +0800

    imported the recorderType and changed the fileExtension and the keys of dataRecordersV1 to uppercase for uniform parameter

--- js/maps/dataRecorderMaps.js
+++ js/maps/dataRecorderMaps.js
@@ -1,9 +1,9 @@
 import { CSVRecorder, JSONRecorder } from "../classes/FileRecorders.js"
-import { CSVDataRecorder } from "../factory-functions/dataRecorder.js"
+import { CSVDataRecorder, recorderType } from "../factory-functions/dataRecorder.js"
 
 const dataRecordersV1 = {
-    csv: () => new CSVRecorder(),
-    json: () => new JSONRecorder(),
+    CSV: () => new CSVRecorder(),
+    JSON: () => new JSONRecorder(),
 }
 
 const dataRecordersV2 = {
@@ -12,15 +12,17 @@ const dataRecordersV2 = {
 
 const dataRecorders = {
     v1: dataRecordersV1,
-    v2: dataRecordersV2,
+    // v2: dataRecordersV2,
+    v2: recorderType,
 }
 
 const dataRecordersMap = (fileName, version = "v1") => {
 
-    const fileExtension = fileName.match(/[^.]*(?=$)/)
+    const fileExtension = fileName
+    .toUpperCase()
+    .match(/[^.]*(?=$)/)
 
     const fileRecorders = dataRecorders[version];
-
     if(!fileRecorders.hasOwnProperty(fileExtension)) {
         console.error(`The fileRecorder.${version} does not provide a fileRecorder for ${fileExtension}`);
         return null;
