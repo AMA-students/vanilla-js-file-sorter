2c706cda7278f3c91ca0a9c476387130f4b2a4bc
Hinrich <johnhinrichggalindo@gmail.com>
2023-02-26 14:19:15 +0800

    minor refactor to make the code more readable

--- js/functions/data-based-sorters/selectionSort.js
+++ js/functions/data-based-sorters/selectionSort.js
@@ -1,56 +1,51 @@
-import { alphanumericComparator, sortingMode, removeUndefined } from "../../classes/utility.js"
+import { alphanumericComparator, sortingMode } from "../../classes/utility.js"
 
 function selectionSort(arr, dataPointIndex, dataRecorder) {
 
-  for (let i = 0; i < arr.length; i++) {
+	for (let i = 0; i < arr.length; i++) {
 
-    // base lowest
-    let lowest = i
+		// base lowest
+		let lowest = i
 
-    const option = { numeric: true, sensitivity: 'base' };
-    const collator = new Intl.Collator(undefined, option);
+		const option = { numeric: true, sensitivity: 'base' };
+		const collator = new Intl.Collator(undefined, option);
 
-    // for loop for determining the actual lowest
-    for (let j = i + 1; j < arr.length; j++) {
-    
-    let currentValue, currentLowest;
+		// for loop for determining the actual lowest
+		for (let j = i + 1; j < arr.length; j++) {
+		
+			let currentValue, currentLowest;
 
-    [currentValue, currentLowest] = sortingMode(
-        arr[j],
-        arr[lowest],
-        dataPointIndex,
-        dataRecorder
-    );
+			[currentValue, currentLowest] = sortingMode(
+				arr[j],
+				arr[lowest],
+				dataPointIndex,
+				dataRecorder
+			);
 
+			// alpanumeric comparison
+			if(typeof(currentValue) === 'string' || typeof(currentLowest) === 'string') {
+				if(alphanumericComparator(currentLowest, currentValue, collator)) {
+					lowest = j
+				}
+				continue;
+			}
 
+			// number to number comparison
+			if ( currentValue < currentLowest ) {
+				lowest = j
+			}
+		}
 
-    //   const [currentValue, currentLowest] = sortingMode(arr[j], arr[lowest], dataPointIndex);
+		// Swap
+		if (lowest !== i) {
+			[arr[i], arr[lowest]] = [arr[lowest], arr[i]]
+		}
+	}
 
-    if(typeof(currentValue) === 'string' || typeof(currentLowest) === 'string') {
-
-        if(alphanumericComparator(currentLowest, currentValue, collator)) {
-          lowest = j
-        }
-
-            continue;
-        }
-
-        if ( currentValue < currentLowest ) {
-            lowest = j
-        }
-    }
-
-    
-    if (lowest !== i) {
-        // Swap
-        [arr[i], arr[lowest]] = [arr[lowest], arr[i]]
-    }
-  }
-
-  dataRecorder?.initializeSortedParsedFileContent()
-  return arr
+	dataRecorder?.initializeSortedParsedFileContent()
+	return arr
 }
 
 export {
-  selectionSort,
+ 	 selectionSort,
 }
